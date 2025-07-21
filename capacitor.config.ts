6248154b4d2e9db1e7274e643d87a0b495bfee6e
Monique Duarte <monique.duarte@selsyn.com.br>
2025-07-21 13:46:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aura',
+  webDir: 'dist'
+};
+
+export default config;
